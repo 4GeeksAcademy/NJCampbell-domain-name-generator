111aa7060018cfa6d9f67e00affd973ffbddac72
Nancy Campbell <124710465+NJCampbell@users.noreply.github.com>
2023-07-11 21:58:42 +0000

    added str output to html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".btn").addEventListener("click", () => {
+    document.querySelector("#theDomainName").innerHTML = generateDomainName();
+  });
 };
+
+let generateDomainName = () => {
+  let pronoun = ["the", "our"];
+  let adjective = ["big", "greatest", "beautiful", "smallest", "blue"];
+  let noun = ["book", "waterbear", "mountain", "mile", "vista"];
+  let extension = [".com", ".edu", ".net"];
+
+  let pronounIndex = Math.floor(Math.random() * pronoun.length);
+  let adjectiveIndex = Math.floor(Math.random() * adjective.length);
+  let nounIndex = Math.floor(Math.random() * noun.length);
+  let extensionIndex = Math.floor(Math.random() * extension.length);
+
+  for (let pronounIndex = 0; pronounIndex < pronoun.length; pronounIndex++) {
+    for (
+      adjectiveIndex = 0;
+      adjectiveIndex < adjective.length;
+      adjectiveIndex++
+    ) {
+      for (let nounIndex = 0; nounIndex < noun.length; nounIndex++) {
+        for (
+          extensionIndex = 0;
+          extensionIndex < extension.length;
+          extensionIndex++
+        );
+      }
+    }
+    return `${pronoun[pronounIndex]} ${adjective[adjectiveIndex]} ${noun[nounIndex]} ${extension[extensionIndex]}`;
+  }
+};
+
+const output = generateDomainName();
